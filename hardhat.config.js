b39c168be0b82a059375d2e1f18fb384d6f01017
wild-mouse <wildmouse0813@gmail.com>
2021-10-08 18:10:34 +0900

    Add verification dependency

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -29,4 +30,7 @@ module.exports = {
       accounts: [process.env.PRIVATE_KEY]
     }
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
